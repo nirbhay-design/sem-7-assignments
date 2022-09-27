57ae3ce158b7c645a30a0826687e1ef90c4cee04
sharma.59 <sharma.59@iitjac.in>
2022-09-27 22:47:29 +0530

    adding files

---
+++ computer_graphics/assn1/README.md
@@ -0,0 +1,84 @@
+## Nirbhay Sharma (B19CSE114)
+## Mayank Raj (B19CSE053)
+## Assignment-1
+
+---
+
+### How to run
+
+**Linux is preferred**
+
+**To run the files the run.sh script is provided**
+
+#### To run que1.cpp (mid point line drawing algorithm)
+
+```
+bash run.sh que1.cpp
+```
+
+*Then enter input in the form of x1 y1 x2 y2 which implies line from (x1, y1) $\to$ (x2, y2)*
+
+#### To run que2.cpp (triangle using bounding box algorithm)
+
+```
+bash run.sh que2.cpp
+```
+
+#### To run que2_triangle_midpt.cpp (triangle using mid point line algorithm)
+
+```
+bash run.sh que2_triangle_midpt.cpp
+```
+
+#### To run que2_supersampling.cpp (triangle using super sampling)
+
+```
+bash run.sh que2_supersampling.cpp
+```
+
+#### To run que3.cpp (circle)
+
+```
+bash run.sh que3.cpp
+```
+
+*The lines are provided just to visualize the angles of 45 and 35 degrees*
+
+
+### Contribution
+
+- Equal contribution
+- Que1 $\to$ Nirbhay Sharma 
+- Que2 bbox $\to$ Nirbhay sharma
+- Que2 triangle using midpoint line algo $\to$ Mayank Raj
+- Que2 supersampling $\to$ Nirbhay sharma, Mayank Raj
+- Que3 $\to$ Mayank Raj
+
+---
+
+<style> 
+
+table, th, td {
+  border: 0.1px solid black;
+  border-collapse: collapse;
+}
+
+* {
+    font-family:"Monaco";
+}
+
+
+h3 {
+    color:#e71989;
+}
+
+h4 {
+    color:purple;
+}
+
+</style>
+
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+<script type="text/x-mathjax-config">
+    MathJax.Hub.Config({ tex2jax: {inlineMath: [['$', '$']]}, messageStyle: "none" });
+</script>
---
+++ opt_for_ml/assn8/que1.py
@@ -0,0 +1,87 @@
+import numpy as np
+import copy
+
+roll_no_last = 4
+call_f = 0;
+call_g = 0;
+
+def calculate_f(x:np.array) -> int:
+    global call_f;
+    call_f += 1
+    return 0.5 * (100 * (x[0] - x[1]**2)**2 + (roll_no_last - x[0]) ** 2)
+
+
+def grad_f(x:np.array, n:int) -> np.array:
+    global call_g;
+    call_g += 1
+    h = 1e-5
+    grad = []
+    f_value = calculate_f(x)
+    for i in range(n):
+        x[i] += h;
+        f_x_h = calculate_f(x)
+        grad_i = (f_x_h - f_value)/h
+        grad.append(grad_i)
+        x[i] -= h
+    return np.array(grad)
+
+def check_armijo_wolf_cond(x_k: np.array, d_k: np.array, alpha:float, beta1:float, beta2:float, n:int) -> bool:
+    xk_alp_dk = x_k + alpha * d_k;
+    f_grad = grad_f(x_k, n);
+    f_grad_alph = grad_f(xk_alp_dk,n)
+    armijo_left = calculate_f(xk_alp_dk);
+    armijo_right = calculate_f(x_k) + alpha * beta1 *np.dot(f_grad.T,d_k);
+
+    wolf_left = np.dot(f_grad_alph.T, d_k)
+    wolf_right = beta2 * np.dot(f_grad.T,d_k);
+
+    return (armijo_left <= armijo_right) and (wolf_left >= wolf_right) 
+
+
+def steepest_direction(x_k:np.array, d_k:np.array, beta1: float, beta2: float, r:float, n:int) -> float:
+    alpha = 1;
+    while not check_armijo_wolf_cond(x_k, d_k, alpha, beta1, beta2, n):
+        alpha = alpha * r
+    return alpha;
+
+def find_x(x_0:np.array, beta1:float, beta2:float, n:int, epsilon:float, r:float) -> np.array:
+    x_k = x_0
+    d_k = -grad_f(x_k, n)
+    norm_grad = np.linalg.norm(-d_k)
+
+    iterations = 0
+    while norm_grad > epsilon:
+        alpha = steepest_direction(x_k, d_k, beta1, beta2, r, n);
+        x_k = x_k + alpha * d_k
+        d_k = -grad_f(x_k,n)
+        norm_grad = np.linalg.norm(-d_k)
+        iterations += 1
+        # print(f"alpha : {alpha}", end="\r")
+        if iterations == 1000:
+            break;
+    return x_k, iterations
+
+n = 2;
+beta1 = 1e-4;
+beta2 = 0.9;
+epsilon = 0.001
+r = 0.5
+x_0 = np.array([2 * roll_no_last,-2 * roll_no_last], dtype=np.float64)
+
+x, iterations = find_x(x_0, beta1, beta2, n, epsilon, r)
+
+# print('\n')
+print(x)
+
+print(f"total_iterations: {iterations}")
+print(f"call_function: {call_f}")
+print(f"call_grad: {call_g}")
+
+
+# x_new = copy.deepcopy(x)
+# x_new[i] += h;
+# print(x_new)
+# f_x_h = calculate_fun_value(x_new)
+# grad_i = (f_x_h - f_value)/h
+# print(f_x_h, grad_i)
+# grad.append(grad_i)
